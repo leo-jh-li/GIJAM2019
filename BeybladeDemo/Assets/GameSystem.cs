977487d6567210da2f61003b4f0c7ce18be470bf
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-09-28 19:55:17 -0400

    Feat: Adding Beyblade 3D to 2D system

--- BeybladeDemo/Assets/GameSystem.cs
+++ BeybladeDemo/Assets/GameSystem.cs
@@ -7,10 +7,6 @@ public class GameSystem : MonoBehaviour {
 	//(0, 3D), (1, 2D)
 	public bool m_gameMode;
 
-	//Players
-	public Beyblade m_p1;
-	public Beyblade m_p2;
-
 	//EventSystems
 	public ClashEvent m_cevent;
 
@@ -18,30 +14,31 @@ public class GameSystem : MonoBehaviour {
 	public void Initiate2D(Beyblade attacker, Beyblade defender) {
 		if (!m_gameMode) {
 			//Disable Controls
-			m_p1.GetComponentInChildren<MovementControls>().enabled = false;
-			m_p1.GetComponentInChildren<TiltControls>().enabled = false;
+			attacker.GetComponentInChildren<MovementControls>().enabled = false;
+			attacker.GetComponentInChildren<TiltControls>().enabled = false;
 
-			m_p2.GetComponentInChildren<MovementControls>().enabled = false;
-			m_p2.GetComponentInChildren<TiltControls>().enabled = false;
+			defender.GetComponentInChildren<MovementControls>().enabled = false;
+			defender.GetComponentInChildren<TiltControls>().enabled = false;
 
 			//Set Velocity to 0
-			m_p1.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
-			m_p2.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+			attacker.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+			defender.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
 
 			//Start the event
 			m_cevent.SetPlayers(attacker.GetComponent<ClashEventModule>(), defender.GetComponent<ClashEventModule>());
+			m_cevent.enabled = true;
 			m_gameMode = !m_gameMode;
 		}
 	}
 
-	public void Initiate3D() {
+	public void Initiate3D(Beyblade p1, Beyblade p2) {
 		if(m_gameMode) {
 			//Enable Controls
-			m_p1.GetComponentInChildren<MovementControls>().enabled = true;
-			m_p1.GetComponentInChildren<TiltControls>().enabled = true;
+			p1.GetComponentInChildren<MovementControls>().enabled = true;
+			p1.GetComponentInChildren<TiltControls>().enabled = true;
 
-			m_p2.GetComponentInChildren<MovementControls>().enabled = true;
-			m_p2.GetComponentInChildren<TiltControls>().enabled = true;
+			p2.GetComponentInChildren<MovementControls>().enabled = true;
+			p2.GetComponentInChildren<TiltControls>().enabled = true;
 			m_gameMode = !m_gameMode;
 		}
 	}
