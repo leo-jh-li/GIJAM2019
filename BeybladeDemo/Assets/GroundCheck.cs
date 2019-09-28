2cf6083c7e697ae9a2a54683457bd6a1af4228f1
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2019-09-28 05:32:05 -0400

    Ground check using existing capsule collider

---
+++ BeybladeDemo/Assets/GroundCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    CapsuleCollider groundCollider;
+
+    HashSet<Collider> groundedOn;
+
+    public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+        groundCollider = GetComponent<CapsuleCollider>();
+        groundedOn = new HashSet<Collider>();
+	}
+	
+	void OnTriggerEnter(Collider c)
+    {
+        groundedOn.Add(c);
+        isGrounded = true;
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        groundedOn.Remove(c);
+        if (groundedOn.Count == 0)
+            isGrounded = false;
+    }
+}
