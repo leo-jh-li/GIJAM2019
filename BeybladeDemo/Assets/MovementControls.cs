75c05cc4194fcd3be627f2f1bc23b1e90b987341
Nanson Zheng <nanson.zheng@mail.utoronto.ca>
2019-09-28 15:07:08 -0400

    Movement calculation done as coroutine

--- BeybladeDemo/Assets/MovementControls.cs
+++ BeybladeDemo/Assets/MovementControls.cs
@@ -8,29 +8,38 @@ public class MovementControls : MonoBehaviour {
 	public float m_smooth = 5f;
 
 	private Rigidbody rb;
+    Vector3 vel;
     GroundCheck ground;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponentInChildren<Rigidbody>();
         ground = GetComponentInChildren<GroundCheck>();
+        StartCoroutine("ApplyVelocity");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 vel = new Vector3((Input.GetKey("d")? 1:0) - (Input.GetKey("a")? 1:0),
+        vel = new Vector3((Input.GetKey("d")? 1:0) - (Input.GetKey("a")? 1:0),
             0,
             (Input.GetKey("w") ? 1 : 0) - (Input.GetKey("s") ? 1 : 0)).normalized * m_maxSpeed;
 
-        vel = Vector3.ProjectOnPlane(vel, ground.groundNormal);
+        
         //print(vel.ToString());
 		// Work on Forward / Backwards Tilt
 
-		rb.velocity = Vector3.Slerp(
-			rb.velocity,
-			vel,
-			Time.deltaTime * m_smooth);
 
 		//print(rb.velocity);
 	}
+
+    IEnumerator ApplyVelocity()
+    {
+        while (true)
+        {
+            rb.velocity = Vector3.Slerp(
+            rb.velocity,
+            Vector3.ProjectOnPlane(vel, ground.groundNormal),
+            Time.deltaTime * m_smooth);
+        }
+    }
 }
