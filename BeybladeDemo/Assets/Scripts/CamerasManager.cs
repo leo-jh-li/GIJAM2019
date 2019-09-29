a11dc955fd836f9becda936333398ed03ec8b381
Leo <leojh.li@mail.utoronto.ca>
2019-09-28 22:13:47 -0400

    transitions with janky angles

--- BeybladeDemo/Assets/Scripts/CamerasManager.cs
+++ BeybladeDemo/Assets/Scripts/CamerasManager.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CamerasManager : MonoBehaviour {
 
+	[SerializeField] private PlayerCam m_leftPlayerCam;
 	[SerializeField] private Camera m_leftCam;
-	[SerializeField] private Vector4 m_leftCamRect;
+	[SerializeField] private Vector4 m_leftDefaultRect;
+	[SerializeField] private PlayerCam m_rightPlayerCam;
 	[SerializeField] private Camera m_rightCam;
-	[SerializeField] private Vector4 m_rightCamRect;
+	[SerializeField] private Vector4 m_rightDefaultRect;
 	[SerializeField] private Vector4 m_fullCamRect;
+	[SerializeField] private float m_transitionSpeed;
 
 	private void Start () {
 		// Set split screen
-		m_leftCam.rect = new Rect(m_leftCamRect.x, m_leftCamRect.y, m_leftCamRect.z, m_leftCamRect.w);
-		m_rightCam.rect = new Rect(m_rightCamRect.x, m_rightCamRect.y, m_rightCamRect.z, m_rightCamRect.w);
+		m_leftCam.rect = new Rect(m_leftDefaultRect.x, m_leftDefaultRect.y, m_leftDefaultRect.z, m_leftDefaultRect.w);
+		m_rightCam.rect = new Rect(m_rightDefaultRect.x, m_rightDefaultRect.y, m_rightDefaultRect.z, m_rightDefaultRect.w);
+	}
+
+	public IEnumerator ResetToSplitScreen() {
+		float timeElapsed = 0;
+		StartCoroutine(m_leftPlayerCam.TransitionToFree());
+		StartCoroutine(m_rightPlayerCam.TransitionToFree());
+		while (m_leftCam.rect.width != m_leftDefaultRect.z) {
+			m_leftCam.rect = new Rect(m_leftDefaultRect.x, m_leftDefaultRect.y, Mathf.Lerp(m_leftCam.rect.width, m_leftDefaultRect.z, timeElapsed * m_transitionSpeed), m_leftDefaultRect.w);
+			m_rightCam.rect = new Rect(Mathf.Lerp(m_rightCam.rect.x, m_rightDefaultRect.x, timeElapsed * m_transitionSpeed), m_rightDefaultRect.y, Mathf.Lerp(m_rightCam.rect.width, m_rightDefaultRect.z, timeElapsed * m_transitionSpeed), m_rightDefaultRect.w);
+			timeElapsed += Time.fixedDeltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public IEnumerator FocusLeftCam() {
+		float timeElapsed = 0;
+		StartCoroutine(m_leftPlayerCam.TransitionToClash(m_rightPlayerCam.followTarget));
+		while (m_leftCam.rect.width < m_fullCamRect.z) {
+			m_leftCam.rect = new Rect(m_leftDefaultRect.x, m_leftDefaultRect.y, Mathf.Lerp(m_leftCam.rect.width, m_fullCamRect.z, timeElapsed * m_transitionSpeed), m_leftDefaultRect.w);
+			m_rightCam.rect = new Rect(Mathf.Lerp(m_rightCam.rect.x, 1, timeElapsed * m_transitionSpeed), m_rightDefaultRect.y, Mathf.Lerp(m_rightCam.rect.width, 0, timeElapsed * m_transitionSpeed), m_rightDefaultRect.w);
+			timeElapsed += Time.fixedDeltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public IEnumerator FocusRightCam() {
+		float timeElapsed = 0;
+		StartCoroutine(m_rightPlayerCam.TransitionToClash(m_leftPlayerCam.followTarget));
+		while (m_rightCam.rect.width < m_fullCamRect.z) {
+			m_rightCam.rect = new Rect(Mathf.Lerp(m_rightCam.rect.x, m_fullCamRect.x, timeElapsed * m_transitionSpeed), m_rightDefaultRect.y, Mathf.Lerp(m_rightCam.rect.width, m_fullCamRect.z, timeElapsed * m_transitionSpeed), m_rightDefaultRect.w);
+			m_leftCam.rect = new Rect(m_leftDefaultRect.x, m_leftDefaultRect.y, Mathf.Lerp(m_leftCam.rect.width, 0, timeElapsed * m_transitionSpeed), m_leftDefaultRect.w);
+			timeElapsed += Time.fixedDeltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	// Buttons for testing
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Keypad0)) {
+			StartCoroutine(ResetToSplitScreen());
+		}
+		if (Input.GetKeyDown(KeyCode.Keypad1)) {
+			StartCoroutine(FocusLeftCam());
+		}
+		if (Input.GetKeyDown(KeyCode.Keypad2)) {
+			StartCoroutine(FocusRightCam());
+		}
 	}
 }
--- BeybladeDemo/Assets/Scripts/PlayerCam.cs
+++ BeybladeDemo/Assets/Scripts/PlayerCam.cs
@@ -2,26 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CameraMode
+{
+	FREE,			// 3D
+	TRANSITIONING,	// Transitioning for 3D to 2D or 2D to 3D
+	CLASH  			// 2D
+}
+
 public class PlayerCam : MonoBehaviour {
 
-	[SerializeField] private Transform m_followTarget;	// The transform to follow
-	[SerializeField] private Transform m_focusTarget;	// The transform to face
+	[SerializeField] private Transform m_cam;
+	public CameraMode camMode = CameraMode.FREE;
+	public Transform followTarget;									// The transform to follow
+	[SerializeField] private Transform m_freeFocusTarget;			// The transform to face
+	private Transform m_clashFocusTarget;
 	[SerializeField] private float m_moveSpeed = 3;
 	[SerializeField] private float m_turnSpeed = 1;
+	[SerializeField] private float m_transitionMoveSpeed = 0.5f;
+	[SerializeField] private float m_transitionFreezeTime = 0.75f;		// Duration after transition to freeze for cool effect
+	[SerializeField] private float m_transitionTime = 3;
+	[SerializeField] private Vector3 m_freePos;						// Camera position for FREE camera
+	private Quaternion m_freeRotation;									// Euler angles for FREE camera
+	[SerializeField] private Vector3 m_clashPos;					// Camera position for CLASH camera
+	[SerializeField] private Vector3 m_clashEulers;					// Euler angles for CLASH camera
+
+	private void Start() {
+		m_freeRotation = m_cam.rotation;
+	}
 
 	private void FixedUpdate()
 	{
-		MoveCam(Time.deltaTime);
+		if (camMode == CameraMode.FREE) {
+			FreeCam(Time.deltaTime, m_moveSpeed);
+		} else if (camMode == CameraMode.CLASH) {
+			ClashCam(Time.deltaTime, m_moveSpeed);
+		}
 	}
 
-	private void MoveCam(float deltaTime)
+	private void FreeCam(float deltaTime, float speed)
 	{
-		if (!(deltaTime > 0) || m_followTarget == null)
+		if (!(deltaTime > 0) || followTarget == null)
 		{
 			return;
 		}
-		transform.position = Vector3.Lerp(transform.position, m_followTarget.position, deltaTime * m_moveSpeed);
-		var rollRotation = Quaternion.LookRotation(m_focusTarget.position - m_followTarget.position, Vector3.up);
-		transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, m_turnSpeed * deltaTime);
+		transform.position = Vector3.Lerp(transform.position, followTarget.position, deltaTime * speed);
+		var rollRotation = Quaternion.LookRotation(m_freeFocusTarget.position - followTarget.position, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, speed * deltaTime);
+	}
+
+	private void ClashCam(float deltaTime, float speed)
+	{
+		if (!(deltaTime > 0) || followTarget == null)
+		{
+			return;
+		}
+		Vector3 midpoint = GetMidpointOnGround(followTarget.position, m_clashFocusTarget.position);
+		Vector2 perpendicularVect = Vector2.Perpendicular(new Vector2(m_clashFocusTarget.position.x - followTarget.position.x, m_clashFocusTarget.position.z - followTarget.position.z));
+		/*
+		Vector3 lookAt = new Vector3(perpendicularVect.x, midpoint.y, perpendicularVect.y);
+		transform.rotation = Quaternion.LookRotation(lookAt);
+		transform.position = Vector3.Lerp(transform.position, midpoint, speed * deltaTime);
+		*/
+		// Lerp camera toward midpoint of attacker and defender
+		m_cam.position = Vector3.Lerp(m_cam.position, new Vector3(midpoint.x, m_cam.position.y, midpoint.z), speed * deltaTime);		
+	}
+
+
+	private Vector3 GetMidpointOnGround(Vector3 a, Vector3 b) {
+		return new Vector3((a.x + b.x) / 2, 0, (a.z + b.z) / 2);
+	}
+
+	public IEnumerator TransitionToClash(Transform defender) {
+		m_clashFocusTarget = defender;
+		camMode = CameraMode.TRANSITIONING;
+		float timeElapsed = 0;
+		while (timeElapsed < m_transitionTime) {
+			Vector3 midpoint = GetMidpointOnGround(followTarget.position, m_clashFocusTarget.position);
+			timeElapsed += Time.fixedDeltaTime;
+			m_cam.position = Vector3.Lerp(m_cam.position, new Vector3(midpoint.x, m_clashPos.y, midpoint.z), timeElapsed / m_transitionTime);
+			m_cam.eulerAngles = Vector3.Lerp(m_cam.eulerAngles, m_clashEulers, timeElapsed / m_transitionTime);
+			yield return new WaitForEndOfFrame();
+		}
+		yield return new WaitForSeconds(m_transitionFreezeTime);
+		camMode = CameraMode.CLASH;
+	}
+
+	public IEnumerator TransitionToFree() {
+		camMode = CameraMode.TRANSITIONING;
+		float timeElapsed = 0;
+		while (timeElapsed < m_transitionTime) {
+			timeElapsed += Time.fixedDeltaTime;
+			m_cam.localPosition = Vector3.Lerp(m_cam.localPosition, m_freePos, timeElapsed / m_transitionTime);
+			// m_cam.eulerAngles = Vector3.Lerp(m_cam.eulerAngles, m_freeEulers, timeElapsed / m_transitionTime);
+			// m_cam.rotation = Quaternion.Slerp(m_cam.rotation, m_freeRotation, timeElapsed / m_transitionTime); 
+			yield return new WaitForEndOfFrame();
+		}
+		m_cam.rotation = m_freeRotation;
+		yield return new WaitForSeconds(m_transitionFreezeTime);
+		camMode = CameraMode.FREE;
 	}
 }
