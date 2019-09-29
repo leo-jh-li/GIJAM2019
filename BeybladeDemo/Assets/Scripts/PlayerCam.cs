cb706f3008d6cb0b14bd582ecf4d165167740fe7
Leo <leojh.li@mail.utoronto.ca>
2019-09-29 00:11:55 -0400

    Finished camera functionality

--- BeybladeDemo/Assets/Scripts/PlayerCam.cs
+++ BeybladeDemo/Assets/Scripts/PlayerCam.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public enum CameraMode
 {
 	FREE,			// 3D
-	TRANSITIONING,	// Transitioning for 3D to 2D or 2D to 3D
+	TRANSITIONING,	// Transitioning from 3D to 2D or 2D to 3D
 	CLASH  			// 2D
 }
 
@@ -19,15 +19,23 @@ public class PlayerCam : MonoBehaviour {
 	[SerializeField] private float m_moveSpeed = 3;
 	[SerializeField] private float m_turnSpeed = 1;
 	[SerializeField] private float m_transitionMoveSpeed = 0.5f;
-	[SerializeField] private float m_transitionFreezeTime = 0.75f;		// Duration after transition to freeze for cool effect
+	[SerializeField] private float m_transitionFreezeTime = 0.75f;	// Duration after transition to freeze for dramatic effect
 	[SerializeField] private float m_transitionTime = 3;
-	[SerializeField] private Vector3 m_freePos;						// Camera position for FREE camera
-	private Quaternion m_freeRotation;									// Euler angles for FREE camera
-	[SerializeField] private Vector3 m_clashPos;					// Camera position for CLASH camera
-	[SerializeField] private Vector3 m_clashEulers;					// Euler angles for CLASH camera
+	private Vector3 m_freePos;										// Position for FREE rig
+	private Quaternion m_freeRotation;								// Rotation for FREE rig
+	private Vector3 m_clashPos;										// Position for CLASH rig
+	private Quaternion m_clashRotation;								// Rotation for CLASH rig
+	[SerializeField] private Transform m_clashRotationRef;			// Reference to transform used for setting m_clashRotation
+	[SerializeField] private float m_clashExtraHeight;				// Extra height added during clash to give more view behind players
+
 
 	private void Start() {
-		m_freeRotation = m_cam.rotation;
+		m_freePos = transform.position;
+		m_freeRotation = transform.rotation;
+		m_clashPos = m_clashRotationRef.position;
+		m_clashRotation = m_clashRotationRef.rotation;
+		// Rotate to account for the camera offset
+		m_clashRotation *= Quaternion.Euler(-m_cam.eulerAngles.x, 0, 0);
 	}
 
 	private void FixedUpdate()
@@ -47,7 +55,7 @@ public class PlayerCam : MonoBehaviour {
 		}
 		transform.position = Vector3.Lerp(transform.position, followTarget.position, deltaTime * speed);
 		var rollRotation = Quaternion.LookRotation(m_freeFocusTarget.position - followTarget.position, Vector3.up);
-		transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, speed * deltaTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rollRotation, deltaTime * speed * m_turnSpeed);
 	}
 
 	private void ClashCam(float deltaTime, float speed)
@@ -57,16 +65,19 @@ public class PlayerCam : MonoBehaviour {
 			return;
 		}
 		Vector3 midpoint = GetMidpointOnGround(followTarget.position, m_clashFocusTarget.position);
-		Vector2 perpendicularVect = Vector2.Perpendicular(new Vector2(m_clashFocusTarget.position.x - followTarget.position.x, m_clashFocusTarget.position.z - followTarget.position.z));
-		/*
-		Vector3 lookAt = new Vector3(perpendicularVect.x, midpoint.y, perpendicularVect.y);
-		transform.rotation = Quaternion.LookRotation(lookAt);
-		transform.position = Vector3.Lerp(transform.position, midpoint, speed * deltaTime);
-		*/
-		// Lerp camera toward midpoint of attacker and defender
-		m_cam.position = Vector3.Lerp(m_cam.position, new Vector3(midpoint.x, m_cam.position.y, midpoint.z), speed * deltaTime);		
+		float playersDist = Vector2.Distance(new Vector2(followTarget.position.x, followTarget.position.z), new Vector2(m_clashFocusTarget.position.x, m_clashFocusTarget.position.z));
+		// Lerp camera toward midpoint of attacker and defender and increase height enough to allow sufficient vision
+		transform.position = Vector3.Lerp(transform.position, new Vector3(midpoint.x, playersDist + m_clashExtraHeight, midpoint.z), speed * deltaTime);
+		KeepAttackerOnLeft(deltaTime, speed);
 	}
 
+	private void KeepAttackerOnLeft(float deltaTime, float speed) {
+		// Align rotation ref to two players
+		m_clashRotationRef.right = m_clashFocusTarget.position - followTarget.position;
+		// Reset m_clashRotationRef's x and z to its original value; we only want to rotate y
+		m_clashRotationRef.rotation = Quaternion.Euler(new Vector3(m_clashRotation.eulerAngles.x, m_clashRotationRef.eulerAngles.y, m_clashRotationRef.eulerAngles.z));
+		transform.rotation = Quaternion.Slerp(transform.rotation, m_clashRotationRef.rotation, speed * deltaTime);
+	}
 
 	private Vector3 GetMidpointOnGround(Vector3 a, Vector3 b) {
 		return new Vector3((a.x + b.x) / 2, 0, (a.z + b.z) / 2);
@@ -76,29 +87,22 @@ public class PlayerCam : MonoBehaviour {
 		m_clashFocusTarget = defender;
 		camMode = CameraMode.TRANSITIONING;
 		float timeElapsed = 0;
-		while (timeElapsed < m_transitionTime) {
-			Vector3 midpoint = GetMidpointOnGround(followTarget.position, m_clashFocusTarget.position);
+		while (timeElapsed < m_transitionTime + m_transitionFreezeTime) {
 			timeElapsed += Time.fixedDeltaTime;
-			m_cam.position = Vector3.Lerp(m_cam.position, new Vector3(midpoint.x, m_clashPos.y, midpoint.z), timeElapsed / m_transitionTime);
-			m_cam.eulerAngles = Vector3.Lerp(m_cam.eulerAngles, m_clashEulers, timeElapsed / m_transitionTime);
+			ClashCam(Time.deltaTime, m_transitionMoveSpeed);
 			yield return new WaitForEndOfFrame();
 		}
-		yield return new WaitForSeconds(m_transitionFreezeTime);
 		camMode = CameraMode.CLASH;
 	}
 
 	public IEnumerator TransitionToFree() {
 		camMode = CameraMode.TRANSITIONING;
 		float timeElapsed = 0;
-		while (timeElapsed < m_transitionTime) {
+		while (timeElapsed < m_transitionTime + m_transitionFreezeTime) {
+			FreeCam(Time.deltaTime, m_transitionMoveSpeed);
 			timeElapsed += Time.fixedDeltaTime;
-			m_cam.localPosition = Vector3.Lerp(m_cam.localPosition, m_freePos, timeElapsed / m_transitionTime);
-			// m_cam.eulerAngles = Vector3.Lerp(m_cam.eulerAngles, m_freeEulers, timeElapsed / m_transitionTime);
-			// m_cam.rotation = Quaternion.Slerp(m_cam.rotation, m_freeRotation, timeElapsed / m_transitionTime); 
 			yield return new WaitForEndOfFrame();
 		}
-		m_cam.rotation = m_freeRotation;
-		yield return new WaitForSeconds(m_transitionFreezeTime);
 		camMode = CameraMode.FREE;
 	}
 }
