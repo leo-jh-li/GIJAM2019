997db7086254a004b6e49dec2f6ba0bb6ac50d25
Leo <lileo.jh+github@gmail.com>
2019-10-01 16:12:44 -0400

    Must be grounded to tilt

--- BeybladeDemo/Assets/Scripts/TiltControls.cs
+++ BeybladeDemo/Assets/Scripts/TiltControls.cs
@@ -12,6 +12,7 @@ public class TiltControls : MonoBehaviour, PlayerControls {
 
 	public string up, down, left, right;
 	
+    GroundCheck ground;
 	bool playerInfluence;
 	int layerMask;
 
@@ -30,44 +31,47 @@ public class TiltControls : MonoBehaviour, PlayerControls {
 	void Start () {
 		playerInfluence = true;
 		layerMask = LayerMask.GetMask("floor");
+        ground = GetComponentInParent<GroundCheck>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float x_tilt = 0;
-		float z_tilt = 0;
+		if (ground.isGrounded) {
+			float x_tilt = 0;
+			float z_tilt = 0;
 
-		// Work on Forward / Backwards Tilt
-		if(Input.GetKey(up) && playerInfluence) {
-			x_tilt = m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
-		}
-		else if(Input.GetKey(down) && playerInfluence) {
-			x_tilt = -1 * m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
-		}
+			// Work on Forward / Backwards Tilt
+			if(Input.GetKey(up) && playerInfluence) {
+				x_tilt = m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
+			}
+			else if(Input.GetKey(down) && playerInfluence) {
+				x_tilt = -1 * m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
+			}
 
-		// Work on Left / Right Tilt
-		if(Input.GetKey(left) && playerInfluence) {
-			z_tilt = m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
-		}
-		else if(Input.GetKey(right) && playerInfluence) {
-			z_tilt = -1 * m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
-		}
+			// Work on Left / Right Tilt
+			if(Input.GetKey(left) && playerInfluence) {
+				z_tilt = m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
+			}
+			else if(Input.GetKey(right) && playerInfluence) {
+				z_tilt = -1 * m_maxTilt * Utilities.sigmoid(m_maxTilt/m_maxTilt * 6, 5);
+			}
 
-		RaycastHit hit;
-		Vector3 downDirection = center.transform.TransformDirection(Vector3.down);
+			RaycastHit hit;
+			Vector3 downDirection = center.transform.TransformDirection(Vector3.down);
 
-		// Debug.DrawLine(center.transform.position, downDirection);
-		if (Physics.Raycast(center.transform.position, downDirection, out hit, 999999f, layerMask)) {
-			// float forwardRotationY = SignedAngleBetween(transform.parent.forward, Vector3.forward, Vector3.up);
-			float forwardRotationY = Vector3.SignedAngle(transform.parent.forward, Vector3.forward, Vector3.up);
-			Vector3 rotatedTiltAngles = (Quaternion.Euler(x_tilt, 0, z_tilt) * Quaternion.Euler(0, forwardRotationY, 0)).eulerAngles;
-			Quaternion baseRotation = Quaternion.LookRotation(hit.normal) * Quaternion.Euler(90, 0, 0);
-			Quaternion targetRotation = Quaternion.Euler(rotatedTiltAngles.x, 0, rotatedTiltAngles.z) * baseRotation;
+			// Debug.DrawLine(center.transform.position, downDirection);
+			if (Physics.Raycast(center.transform.position, downDirection, out hit, 999999f, layerMask)) {
+				// float forwardRotationY = SignedAngleBetween(transform.parent.forward, Vector3.forward, Vector3.up);
+				float forwardRotationY = Vector3.SignedAngle(transform.parent.forward, Vector3.forward, Vector3.up);
+				Vector3 rotatedTiltAngles = (Quaternion.Euler(x_tilt, 0, z_tilt) * Quaternion.Euler(0, forwardRotationY, 0)).eulerAngles;
+				Quaternion baseRotation = Quaternion.LookRotation(hit.normal) * Quaternion.Euler(90, 0, 0);
+				Quaternion targetRotation = Quaternion.Euler(rotatedTiltAngles.x, 0, rotatedTiltAngles.z) * baseRotation;
 
-			transform.rotation = Quaternion.Slerp(
-			transform.rotation,
-			targetRotation,
-			Time.deltaTime * m_smooth);
+				transform.rotation = Quaternion.Slerp(
+					transform.rotation,
+					targetRotation,
+					Time.deltaTime * m_smooth);
+			}
 		}
 	}
 }
